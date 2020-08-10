1e2540240da5dd698b910fb9c2cf8f23a984e75b
ilaydabykakova <ilaydabuyukakova27@gmail.com>
2020-08-10 14:43:19 +0300

    new

---
+++ unpak-first.php
@@ -0,0 +1,20 @@
+<!doctype html>
+<html lang="tr" >
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta http-equiv="refresh" content="10";>
+
+    <title>Login</title>
+    <style media="screen">
+      .container{padding: :30px;}
+    </style>
+  </head>
+  <body>
+    <div class="container">
+      <embed src="files/unpak-menu.pdf.pdf#toolbar" type="application/pdf" width="100%" height="600px"/>
+    </div>
+
+  </body>
+
+</html>
