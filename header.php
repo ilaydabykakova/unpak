16c5b01bd718a775b08c228e84dc00ee57e000c7
ilaydabykakova <ilaydabuyukakova27@gmail.com>
2020-08-20 20:50:52 +0300

    commit new

---
+++ header.php
@@ -0,0 +1,19 @@
+<div class="row design-index">
+
+
+  <div id="mySidenav" class="sidenav" >
+  <a href="javascript:void(0)" class="closebtn" onclick="closeNav()">&times;</a>
+
+  <a href="kahvaltilar.php" onclick="closeNav()" <span style="font-size:20px; ">Kahvaltılar</span></a>
+  <a href="#"><span style="font-size:20px;">Başlangıçlar & Sandiviçler </span></a>
+  <a href="#"><span style="font-size:20px;">Salatalar</span></a>
+  <a href="#"><span style="font-size:20px;">Hamburgerler,Wraplar,Pizzalar/Pideler,Makarnalar</span></a>
+  <a href="#"><span style="font-size:20px;">Yemekler</span></a>
+  <a href="#"><span style="font-size:20px;">Pastalar, Tuzlu & Tatlı Atıştırmalıklar ,Tatlılar </span></a>
+  <a href="#"><span style="font-size:20px;">İçecekler</span></a>
+</div>
+
+<span class="text-style" onclick="openNav()">&#9776; MENÜ</span>
+
+
+</div>
---
+++ kahvaltilar.php
@@ -0,0 +1,106 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <!-- <meta http-equiv="refresh" content="10";> -->
+ <link rel="stylesheet" href="css/bootstrap.min.css" crossorigin="anonymous">
+ <link rel="stylesheet" href="css/style-menu.css" crossorigin="anonymous">
+ <style media="screen">
+ #carouselExampleIndicators{
+width:100%;
+margin: 0 auto;
+
+}
+.carousel-item {
+  height: 75vh;
+background: no-repeat center  scroll;
+-webkit-background-size: cover;
+ -moz-background-size: cover;
+-o-background-size: cover;
+background-size: cover;
+}
+ </style>
+<style media="screen">
+
+</style>
+
+  <title>Main</title>
+</head>
+
+<body>
+    <div class="container">
+
+        <?php include('header.php')?>
+
+      <div class="row design-index2" >
+
+      <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+
+
+          <div class="carousel-inner" role="listbox">
+            <!-- Slide One - Set the background image for this slide in the line below -->
+            <div class="carousel-item active d-block w-100" style="background-image: url('files/Sayfa 1-1.jpg')">
+
+            </div>
+            <!-- Slide Two - Set the background image for this slide in the line below -->
+            <div class="carousel-item " style="background-image: url('files/Sayfa 2-2.jpg')">
+
+            </div>
+            <div class="carousel-item" style="background-image: url('files/Sayfa 3-1.jpg')">
+
+            </div>
+            <div class="carousel-item" style="background-image: url('files/Sayfa 3-2.jpg')">
+
+            </div>
+            <div class="carousel-item" style="background-image: url('files/Sayfa 4-1.jpg')">
+
+            </div>
+            <div class="carousel-item" style="background-image: url('files/Sayfa 4-2.jpg')">
+
+            </div>
+            <div class="carousel-item" style="background-image: url('files/Sayfa 5-1.jpg')">
+
+            </div>
+            <div class="carousel-item" style="background-image: url('files/Sayfa 5-2.jpg')">
+
+            </div>
+            <div class="carousel-item" style="background-image: url('files/Sayfa 6-1.jpg')">
+
+            </div>
+            <div class="carousel-item" style="background-image: url('files/Sayfa 6-2.jpg')">
+
+        </div>
+          <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="sr-only">Previous</span>
+          </a>
+          <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="sr-only">Next</span>
+          </a>
+        </div>
+        </div>
+
+    </div>
+
+    <script>
+    function openNav() {
+      document.getElementById("mySidenav").style.width = "95%";
+    }
+
+    function closeNav() {
+      document.getElementById("mySidenav").style.width = "0";
+    }
+    </script>
+        <script src="js/jquery-3.5.1.min.js"  crossorigin="anonymous"></script>
+        <script src="js/bootstrap.min.js" crossorigin="anonymous"></script>
+    <script src="js/jquery-3.5.1.min.js"  crossorigin="anonymous"></script>
+    <script src="js/bootstrap.min.js" crossorigin="anonymous"></script>
+
+
+</body>
+
+<footer> </footer>
+
+</html>
